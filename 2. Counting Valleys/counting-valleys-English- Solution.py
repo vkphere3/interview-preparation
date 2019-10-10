2e1ac9463e02ca57b3de46db826ea0563ffd1ad2
VIKASH KUMAR <vikash@akoya.org>
2019-10-10 23:13:35 +0530

    no message

---
+++ 2. Counting Valleys/counting-valleys-English- Solution.py
@@ -0,0 +1,7 @@
+Probability and Statistics
+
+	Mean, Median, Mode, Standard Deviation, Variance
+	http://www.ltcconline.net/greenl/courses/201/descstat/mean.htm
+	
+	Sampling mean of random samples
+	https://bolt.mph.ufl.edu/6050-6052/module-9/sampling-distribution-of-x-bar/
